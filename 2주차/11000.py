bacf738663c91b6efa6365e390f6df080830cac2
alsdud0810 <100515104+alsdud0810@users.noreply.github.com>
2022-04-27 18:50:23 +0900

    Update 11000.py

--- 2주차/11000.py
+++ 2주차/11000.py
@@ -14,7 +14,7 @@ room=[]
 heapq.heappush(room,arr[0][1]) #첫번째 강의 종료 시간
 
 for i in range(1,n):
-    if arr[i][0]<room[0]: #i번째 강의 종료 시간보다 다음 강의 시작시간이 빠르면-> 방 하나 만들기
+    if arr[i][0]<room[0]:
         heapq.heappush(room,arr[i][1])
     else: #아니면, 바로 이어서 강의 -> 방 만들 필요 없음.
         heapq.heappop(room)
